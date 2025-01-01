7a363d795a5049641167ff7ce9569f6597698485
miguel112107 <maflores1108@gmail.com>
2024-12-31 17:04:28 -0700

    meta tag addition

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,11 +2,19 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Ervotech offers expert web consulting, including SEO, web design, and software development tailored to your business needs.">
+    <meta name="keywords" content="Web Consulting, SEO Services, Web Development, Website Migration, Software Development">
+    <meta name="author" content="Ervotech">
+    <meta property="og:title" content="Transform Your Web Presence with Ervotech">
+    <meta property="og:description" content="Modern web solutions for SEO, web design, and software development.">
+    <!-- <meta property="og:image" content="images/Logo2.png"> -->
+    <meta property="og:url" content="www.ervotechep.com">
+    <meta property="og:type" content="website">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="icon" href="favicon.ico">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
     <title inertia>{{ config('app.name', '') }}</title>
 
     <!-- Fonts -->
